39b265b4e0872c4e9847656a6f15543b88c67d64
tommy-atkins <thomas.robert.atkins@gmail.com>
2022-08-01 17:07:33 +0800

    2. Smoke Test

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,6 +2,10 @@ require("@nomiclabs/hardhat-waffle");
 require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
+
 module.exports = {
   solidity: "0.8.9",
+  networks: {
+    localhost: {}
+  },
 };
