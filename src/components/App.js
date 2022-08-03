5095970ee59ba5b981ec3f315ea79ca0d92f08c5
tommy-atkins <thomas.robert.atkins@gmail.com>
2022-08-03 15:31:19 +0800

    18. Navbar

--- src/components/App.js
+++ src/components/App.js
@@ -10,6 +10,8 @@ import {
   loadExchange 
 } from '../store/interactions';
 
+import Navbar from './Navbar';
+
 function App() {
 
   const dispatch = useDispatch()
@@ -20,8 +22,15 @@ function App() {
     // Fetch current network's chainId (e.g. hardhat: 31337, kovan: 42)
     const chainId = await loadNetwork(provider, dispatch)
 
-    // Fetch current account & balance from Metamask
-    await loadAccount(dispatch, provider)
+    // Reload page when network changes
+    window.ethereum.on('chainChanged', () => {
+      window.location.reload()
+    })
+
+    // Fetch current account & balance from Metamask when accounts changed
+    window.ethereum.on('accountsChanged', () => {
+      loadAccount(dispatch, provider)
+    })
 
     // Load Token Smart Contracts
     const digitalNomad = config[chainId].digitalNomad
@@ -40,7 +49,7 @@ function App() {
   return (
     <div>
 
-      {/* Navbar */}
+      <Navbar />
 
       <main className='exchange grid'>
         <section className='exchange__section--left grid'>
