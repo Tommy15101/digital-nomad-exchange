2abd337e9cabc39df91496cab3dc1423b7bbc035
tommy-atkins <thomas.robert.atkins@gmail.com>
2022-08-05 18:38:24 +0800

    Deployment Phase

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,11 +1,16 @@
 require("@nomiclabs/hardhat-waffle");
 require("dotenv").config();
+const privateKeys = process.env.PRIVATE_KEYS || ""
 
 /** @type import('hardhat/config').HardhatUserConfig */
 
 module.exports = {
   solidity: "0.8.9",
   networks: {
-    localhost: {}
+    localhost: {},
+    kovan: {
+      url: `https://kovan.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      accounts: privateKeys.split(','),
+    }
   },
 };
